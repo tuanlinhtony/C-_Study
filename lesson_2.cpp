81b45adec6cd115eacd25176bf3726898d76720a
tuanlinhtony <mrpi3990@gmail.com>
2021-08-07 02:31:59 +0800

    lesson 2

---
+++ lesson_2.cpp
@@ -0,0 +1,13 @@
+// Write a program to calculate the sum of 12 and 15 and display the result on the screen.
+#include <iostream>
+//iostream is standard library provides basic input and output services for C++ programs.
+//https://en.wikipedia.org/wiki/Input/output_(C%2B%2B)
+using namespace std;
+//“using namespace std” means we use the namespace named std. “std” is an abbreviation for standard.
+//https://medium.com/breaktheloop/why-using-namespace-std-is-used-after-including-iostream-dc5ae45db652
+int main(){
+    cout << 12 + 15;
+    return 0;
+    //A return 0 means that the program will execute successfully and did what it was intended to do.
+    //https://www.geeksforgeeks.org/return-0-vs-return-1-in-c/
+}
