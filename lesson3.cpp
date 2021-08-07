07949c71f6c0e28000eab4d9f95d2e316afb8faa
tuanlinhtony <mrpi3990@gmail.com>
2021-08-07 09:49:37 +0800

    Dislay the following question on the screen

---
+++ lesson3.cpp
@@ -0,0 +1,8 @@
+// Write a program to calculate the sum of 1489 and 7348 and print result on the screen.
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 1489 + 7348;
+    return 0;
+}
---
+++ lesson4.cpp
@@ -0,0 +1,8 @@
+// Write a program to print the following line on the screen: 324 + 553 = {P}
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "324 + 553 = " << 324+553;
+    return 0;
+}
